cf9dadf887f0b967485d79a2cfd5711e0ea9164e
TomasVysinsky <vysinsky2@stud.uniza.sk>
2023-01-04 10:18:05 +0100

    Creating new project

---
+++ cmake-build-debug/CMakeFiles/downloadManager.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/downloadManager.dir/main.c.o"
+  "downloadManager"
+  "downloadManager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/downloadManager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

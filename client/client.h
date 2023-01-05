a60242fe1bda06b4008997e0a6b94c3eec633237
kivaderf <filip243@gmail.com>
2023-01-05 15:51:56 +0100

    new FTP client

--- client/client.h
+++ client/client.h
@@ -1,10 +1,8 @@
-#include <stdio.h>
-
-#ifndef CLIENT
-#define CLIENT
-
-void mainClient(char * hostname);
-void tcpClient(char * hostname);
-void ftpClient(char * hostname);
-
-#endif
+//#include <stdio.h>
+//
+//#ifndef CLIENT
+//#define CLIENT
+//
+//void mainClient(char * hostname);
+//
+//#endif
